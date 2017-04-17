e2e7be0b3d4410c84338ea64c4ba727f7bd3f650
elliottabirch <elliottabirch@gmail.com>
2017-04-17 01:18:12 -0700

    finish quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,19 @@
 function quickSort(input) {
-
+  if (end - start > 1) {
+    const val = input[start];
+    let swapIndex = start + 1;
+    for (var i = start + 1; i <= end; i++) {
+      if (input[i] < val) {
+        const swapValue = input[swapIndex];
+        input[swapIndex] = input[i];
+        input[i] = swapValue;
+        swapIndex++;
+      }
+    }
+    input[start] = input[swapIndex - 1];
+    input[swapIndex - 1] = val;
+    quicksort(input, start, swapIndex - 1);
+    quicksort(input, swapIndex, end);
+  }
+  return input;
 }
